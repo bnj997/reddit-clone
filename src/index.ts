36cc141add75283583395033b5649a7c112c2754
bnj997 <bnj997@gmail.com>
2020-08-25 14:24:24 +1000

    Setted up basic environment for backend using Graphql, MikroORM, typescript, Express and some basic CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,34 @@
+import 'reflect-metadata';
 import {MikroORM} from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express'
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql';
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   //connect to database
   const orm = await MikroORM.init(microConfig);
   //run migrations
   orm.getMigrator().up();
-  //Run SQL stuff
-  // const post = orm.em.create(Post, {title: 'my first post'})
-  // await orm.em.persistAndFlush(post)
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts)
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({em: orm.em})
+  });
+
+  //create graphql endpoint for us in express
+  apolloServer.applyMiddleware({app});
+
+  app.listen(5000, () => {
+    console.log('server started at localhost:5000')
+  });
 }
 
 main();
---
+++ src/resolvers/post.ts
@@ -0,0 +1,66 @@
+import { Resolver, Query, Ctx, Int, Arg, Mutation } from "type-graphql";
+import { Post } from "../entities/Post";
+import { MyContext } from "src/types";
+
+@Resolver()
+export class PostResolver {
+  
+  //Get All Posts
+  @Query(() => [Post])
+  getAllPosts(@Ctx() {em}: MyContext): Promise<Post[]> {
+    return em.find(Post, {})
+  }
+
+  //Get a Single Post
+  @Query(() => Post, {nullable: true})
+  getASinglePost(
+    @Arg('id', () => Int) id: number,    
+    @Ctx() {em}: MyContext
+    ): Promise<Post | null> {
+    return em.findOne(Post, { id })
+  }
+
+  //Create Post
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,    
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, {title});
+    await em.persistAndFlush(post)
+    return post
+  }
+
+  //Update Post
+  @Mutation(() => Post, {nullable: true})
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, {nullable: true}) title: string,      
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, {id});
+    if(!post) {
+      return null
+    }
+    if (typeof title !== 'undefined') {
+      post.title = title
+      await em.persistAndFlush(post)
+    }
+    return post
+  }
+
+  //Delete Post
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,    
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    try {
+      await em.nativeDelete(Post, {id})
+    } catch {
+      return false;
+    }
+    return true;
+  }
+
+}
