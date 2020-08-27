aaa34de7850b9be3fa4f957289ba7e3290cea8e6
bnj997 <bnj997@gmail.com>
2020-08-27 14:53:19 +1000

    Cleared cache so that login updates the navbar with the name of user

--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -1,14 +1,66 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
-import {Provider, createClient} from 'urql'
+import { Provider, createClient, dedupExchange, fetchExchange} from 'urql'
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
+import theme from '../theme'
+import { GetCurrentUserDocument, GetCurrentUserQuery, LoginMutation, RegisterMutation } from '../generated/graphql';
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          loginUser: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, GetCurrentUserQuery>(
+              cache,
+              { query: GetCurrentUserDocument },
+              _result,
+              (result, query) => {
+                if (result.loginUser.errors) {
+                  return query;
+                } else {
+                  return {
+                    getCurrentUser: result.loginUser.user,
+                  };
+                }
+              }
+            );
+          },
+          registerUser: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, GetCurrentUserQuery>(
+              cache,
+              { query: GetCurrentUserDocument },
+              _result,
+              (result, query) => {
+                if (result.registerUser.errors) {
+                  return query;
+                } else {
+                  return {
+                    getCurrentUser: result.registerUser.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
-
-import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
