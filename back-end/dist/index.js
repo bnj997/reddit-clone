a858c558de7e4d220bdb086c1225c73a2d37f47d
bnj997 <bnj997@gmail.com>
2020-09-01 18:48:12 +1000

    minor changes

--- back-end/dist/index.js
+++ back-end/dist/index.js
@@ -26,7 +26,6 @@ const cors_1 = __importDefault(require("cors"));
 const typeorm_1 = require("typeorm");
 const Post_1 = require("./entities/Post");
 const User_1 = require("./entities/User");
-const path_1 = __importDefault(require("path"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const conn = yield typeorm_1.createConnection({
         type: 'postgres',
@@ -35,7 +34,6 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         password: 'postgres',
         logging: true,
         synchronize: true,
-        migrations: [path_1.default.join(__dirname, "./migrations/*")],
         entities: [Post_1.Post, User_1.User]
     });
     const app = express_1.default();
